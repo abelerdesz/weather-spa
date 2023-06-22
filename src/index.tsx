a43feb9aee59318f61536af043693e2d35ca49c7
Ábel Erdész <abel.erdesz@gmail.com>
2023-06-22 16:14:22 +0200

    feat: add responsive font sizes

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { ThemeProvider } from '@emotion/react'
-import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { store } from './store'
 import { theme } from './theme'
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
